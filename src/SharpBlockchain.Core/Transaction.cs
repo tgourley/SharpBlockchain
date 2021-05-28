5d0bffd50a08b16cb69d743e11f3405751ffadd9
Trey Gourley <trey.gourley@trustasc.com>
2021-05-28 16:07:12 -0400

    Implement blockchain example

---
+++ src/SharpBlockchain.Core/Transaction.cs
@@ -0,0 +1,18 @@
+﻿namespace SharpBlockchain.Core
+{
+    public class Transaction
+    {
+        public string From { get; }
+
+        public string To { get; }
+
+        public double Amount { get; }
+
+        public Transaction(string from, string to, double amount)
+        {
+            From = from;
+            To = to;
+            Amount = amount;
+        }
+    }
+}
